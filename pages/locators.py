4920edd861e91ec699d068607baa56704c098b8c
BeGitMot <bgmot@list.ru>
2019-10-24 11:39:25 +0300

    added parametrized test

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket");
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1");
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color");
+    BASKET  = (By.CSS_SELECTOR, "div.basket-mini");
+
+    def GetAddedToCartSelector(product_name):
+        return (By.XPATH, f"//strong[text()=\"{product_name}\"]");
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def should_be_add_to_cart_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_BUTTON), "Add to cart button is not presented"
+
+    def add_to_cart(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_button.click()
+
+    def check_product_added_to_cart(self):
+        product_name = self.get_product_name()
+        assert self.is_element_present(*ProductPageLocators.GetAddedToCartSelector(product_name)), "Product is not added to cart"
+
+    def check_product_price_in_cart(self):
+        price = self.get_product_price()
+        cart_price = self.get_basket_price()
+        assert price in cart_price, "Wrong price"
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def get_basket_price(self):
+        return self.browser.find_element(*ProductPageLocators.BASKET).text
+
+
